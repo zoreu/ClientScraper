a2953a4ac41f9355fec51cb7d3c306d84b593c9b
Joel Silva <53149347+zoreu@users.noreply.github.com>
2024-08-11 17:58:16 -0300

    Update ClientScraper.py

--- ClientScraper.py
+++ ClientScraper.py
@@ -10,14 +10,18 @@ logger = logging.getLogger(__name__)
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 PROXY = 'https://webproxy.liyao.blog/'
+PROXY2 = 'https://webproxy.joelsinhox.workers.dev/'
 
 class cfscraper:
     session = requests.Session()
     
     @classmethod
-    def get(cls, url, headers={}, timeout=None, allow_redirects=True, cookies={}):
+    def get(cls, url, headers={}, timeout=None, allow_redirects=True, cookies={},direct=True):
         sess = cls.session
         proxy_url = PROXY + url
+        proxy_url2 = PROXY2 + url
+        if not direct:
+            url = proxy_url
         if not headers:
             headers = {'User-Agent': USER_AGENT}
         try:
@@ -35,6 +39,18 @@ class cfscraper:
                         logger.error("Erro 403: Access denied")
                     elif err.response.status_code == 503:
                         logger.error("Erro 503: Service unavailable.")
+                    elif err.response.status_code == 429:
+                        try:
+                            res = sess.get(proxy_url2, headers=headers, cookies=cookies, allow_redirects=allow_redirects, timeout=timeout)
+                            res.raise_for_status()            
+                            return res
+                        except requests.exceptions.HTTPError as err:
+                            if err.response.status_code == 403:
+                                logger.error("Erro 403: Access denied")
+                            elif err.response.status_code == 503:
+                                logger.error("Erro 503: Service unavailable.")                                                 
+                            else:
+                                logger.error("HTTP error occurred: {0}".format(err))
                     else:
                         logger.error("HTTP error occurred: {0}".format(err))
             else:
@@ -49,6 +65,18 @@ class cfscraper:
                     logger.error("Erro 403: Access denied")
                 elif err.response.status_code == 503:
                     logger.error("Erro 503: Service unavailable.")
+                elif err.response.status_code == 429:
+                    try:
+                        res = sess.get(proxy_url2, headers=headers, cookies=cookies, allow_redirects=allow_redirects, timeout=timeout)
+                        res.raise_for_status()            
+                        return res
+                    except requests.exceptions.HTTPError as err:
+                        if err.response.status_code == 403:
+                            logger.error("Erro 403: Access denied")
+                        elif err.response.status_code == 503:
+                            logger.error("Erro 503: Service unavailable.")                                                 
+                        else:
+                            logger.error("HTTP error occurred: {0}".format(err))                 
                 else:
                     logger.error("HTTP error occurred: {0}".format(err))
             except Exception as e:
@@ -57,9 +85,12 @@ class cfscraper:
         return None
 
     @classmethod
-    def post(cls, url, headers={}, timeout=None, data=None, json=None, allow_redirects=True):
+    def post(cls, url, headers={}, timeout=None, data=None, json=None, allow_redirects=True, direct=True):
         sess = cls.session
         proxy_url = PROXY + url
+        proxy_url2 = PROXY2 + url
+        if not direct:
+            url = proxy_url        
         if not headers:
             headers = {'User-Agent': USER_AGENT}
         try:
@@ -83,6 +114,21 @@ class cfscraper:
                         logger.error("Erro 403: Access denied")
                     elif err.response.status_code == 503:
                         logger.error("Erro 503: Service unavailable.")
+                    elif err.response.status_code == 429:
+                        try:
+                            if data:
+                                res = sess.post(proxy_url2, headers=headers, data=data, allow_redirects=allow_redirects, timeout=timeout)
+                            else:
+                                res = sess.post(proxy_url2, headers=headers, json=json, allow_redirects=allow_redirects, timeout=timeout)
+                            res.raise_for_status()            
+                            return res
+                        except requests.exceptions.HTTPError as err:
+                            if err.response.status_code == 403:
+                                logger.error("Erro 403: Access denied")
+                            elif err.response.status_code == 503:
+                                logger.error("Erro 503: Service unavailable.")
+                            else:
+                                logger.error("HTTP error occurred: {0}".format(err))
                     else:
                         logger.error("HTTP error occurred: {0}".format(err))
             else:
@@ -100,10 +146,24 @@ class cfscraper:
                     logger.error("Erro 403: Access denied")
                 elif err.response.status_code == 503:
                     logger.error("Erro 503: Service unavailable.")
+                elif err.response.status_code == 429:
+                    try:
+                        if data:
+                            res = sess.post(proxy_url2, headers=headers, data=data, allow_redirects=allow_redirects, timeout=timeout)
+                        else:
+                            res = sess.post(proxy_url2, headers=headers, json=json, allow_redirects=allow_redirects, timeout=timeout)
+                        res.raise_for_status()            
+                        return res
+                    except requests.exceptions.HTTPError as err:
+                        if err.response.status_code == 403:
+                            logger.error("Erro 403: Access denied")
+                        elif err.response.status_code == 503:
+                            logger.error("Erro 503: Service unavailable.")
+                        else:
+                            logger.error("HTTP error occurred: {0}".format(err))
                 else:
                     logger.error("HTTP error occurred: {0}".format(err))
             except Exception as e:
                 logger.error("HTTP error occurred: {0}".format(e))
         return None 
                                 
-
